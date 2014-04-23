f4fb6dc3cb2f162869e666ec1f3236142e6016c4
Marco Wiedemeyer <mail@mwiedemeyer.de>
2014-04-23 13:57:32 +0200

    Removed DE specific datepicker stuff

--- SPForms/SPForms_Main.ts
+++ SPForms/SPForms_Main.ts
@@ -33,8 +33,6 @@ module SPForms {
         private initialize(): void {
             this.settings = JSON.parse(this.form.attr("data-form-settings"));
 
-            this.initJQueryUIDefaults();
-
             // initialize all form fields
             this.form.find("[data-form-field]").each((i, f) => {
                 var field = FormFields.FormField.getFormFieldByType($(f));
@@ -207,63 +205,6 @@ module SPForms {
                 (sender, args) => {
                     deferred.reject(args.get_message());
                 });
-        }
-
-        private initJQueryUIDefaults() {
-            $.datepicker.regional["de"] = {
-                clearText: "löschen",
-                clearStatus: "aktuelles Datum löschen",
-                closeText: "schließen",
-                closeStatus: "ohne Änderungen schließen",
-                prevText: "Zurück",
-                prevStatus: "letzten Monat zeigen",
-                nextText: "Vor",
-                nextStatus: "nächsten Monat zeigen",
-                currentText: "heute",
-                currentStatus: "",
-                monthNames: [
-                    "Januar",
-                    "Februar",
-                    "März",
-                    "April",
-                    "Mai",
-                    "Juni",
-                    "Juli",
-                    "August",
-                    "September",
-                    "Oktober",
-                    "November",
-                    "Dezember"
-                ],
-                monthNamesShort: [
-                    "Jan",
-                    "Feb",
-                    "Mär",
-                    "Apr",
-                    "Mai",
-                    "Jun",
-                    "Jul",
-                    "Aug",
-                    "Sep",
-                    "Okt",
-                    "Nov",
-                    "Dez"
-                ],
-                monthStatus: "anderen Monat anzeigen",
-                yearStatus: "anderes Jahr anzeigen",
-                weekHeader: "Wo",
-                weekStatus: "Woche des Monats",
-                dayNames: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
-                dayNamesShort: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
-                dayNamesMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
-                dayStatus: "Setze DD als ersten Wochentag",
-                dateStatus: "Wähle D, M d",
-                dateFormat: "dd.mm.yy",
-                firstDay: 1,
-                initStatus: "Datum auswählen",
-                isRTL: false
-            };
-            $.datepicker.setDefaults($.datepicker.regional["de"]);
         }
     }
 
